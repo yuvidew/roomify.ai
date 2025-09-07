e0d2ff8f77df7d35503ece8408002ee7fb03a551
Yuvraj dewangan <yd00102@gmail.com>
2025-09-07 06:59:32 +0530

    update: changing a project id envorment variable

--- src/lib/session-middleware.ts
+++ src/lib/session-middleware.ts
@@ -32,7 +32,7 @@ export const sessionMiddleware = createMiddleware<AdditionalContext>(
 
     const client = new Client()
       .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
 
     const session = getCookie(c, AUTH_COOKIE);
 
