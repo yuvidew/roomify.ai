4e3a43ffc50b557f7f6bfefe2effea661a024930
Yuvraj dewangan <yd00102@gmail.com>
2025-10-02 05:03:43 +0530

    update : updating a side bar item and adding scroll area when the sidebar item is grater then 6 thn show the scroll bar

--- src/components/side-bar/nav-documents.tsx
+++ src/components/side-bar/nav-documents.tsx
@@ -25,6 +25,7 @@ import { useDeleteHomeDocument } from "@/features/dashboard/api/use-delete-home-
 import Spinner from "../Spinner"
 import { useMemo, useState } from "react"
 import { Hint } from "../Hint"
+import { ScrollArea } from "../ui/scroll-area"
 // import { ScrollArea } from "../ui/scroll-area"
 
 export const NavDocuments = () => {
@@ -47,6 +48,8 @@ export const NavDocuments = () => {
         return showAll ? documents : documents.slice(0, 5)
     }, [documents, showAll])
 
+    const shouldEnableScroll = visibleDocuments.length > 6
+    const scrollAreaClasses = shouldEnableScroll ? "max-h-72 pr-1" : "pr-1"
 
     if (isError) {
         return null
@@ -60,70 +63,74 @@ export const NavDocuments = () => {
             {/* <ScrollArea className=" h-60 w-80"> */}
 
             <SidebarMenu>
-                {isLoading ? (
-                    [1, 2, 3, 4].map((item) => (
-                        <Skeleton key={item} className=" h-8 rounded-sm" />
-                    ))
-                ) :
-                    documents.length > 0 ? visibleDocuments.map((item) => (
-                        <SidebarMenuItem key={item.$id}>
-                            <Hint side = "left" align="center" label={item.home_title}>
-                            <SidebarMenuButton asChild isActive={item.$id === id}>
-                                <Link href={`/${item.$id}`}>
-                                    <House />
-                                    <span className=" w-[80%] truncate">{item.home_title}</span>
-                                </Link>
-                            </SidebarMenuButton>
-                            </Hint>
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <SidebarMenuAction
-                                        showOnHover
-                                        className="data-[state=open]:bg-accent rounded-sm"
-                                    >
-                                        <EllipsisVertical />
-                                        <span className="sr-only">More</span>
-                                    </SidebarMenuAction>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent
-                                    className="w-24 rounded-lg"
-                                    side={isMobile ? "bottom" : "right"}
-                                    align={isMobile ? "end" : "start"}
-                                >
-                                    <DropdownMenuItem onClick={() => router.push(`/${item.$id}`)}>
-                                        <Folder />
-                                        <span>Open</span>
-                                    </DropdownMenuItem>
-                                    <DropdownMenuSeparator />
-                                    <DropdownMenuItem
-                                        variant="destructive"
-                                        onClick={() =>
-                                            mutate(
-                                                { param: { extract_room_id: item.$id } },
-                                                {
-                                                    onSuccess: () => router.back()
+                <ScrollArea className={scrollAreaClasses}>
+                    <div className="flex flex-col gap-3">
+                        {isLoading ? (
+                            [1, 2, 3, 4].map((item) => (
+                                <Skeleton key={item} className=" h-8 rounded-sm" />
+                            ))
+                        ) :
+                            documents.length > 0 ? visibleDocuments.map((item) => (
+                                <SidebarMenuItem key={item.$id} className={showAll ?"w-[60%] lg:w-[57%] md:w-[56%]" : "w-[64%] lg:w-[61.5%] md:w-[61.5%]"}>
+                                    <Hint side="left" align="center" label={item.home_title}>
+                                        <SidebarMenuButton asChild isActive={item.$id === id}>
+                                            <Link href={`/${item.$id}`}>
+                                                <House />
+                                                <span className=" w-[85%] truncate">{item.home_title}</span>
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </Hint>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <SidebarMenuAction
+                                                showOnHover
+                                                className="data-[state=open]:bg-accent rounded-sm"
+                                            >
+                                                <EllipsisVertical />
+                                                <span className="sr-only">More</span>
+                                            </SidebarMenuAction>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent
+                                            className="w-24 rounded-lg"
+                                            side={isMobile ? "bottom" : "right"}
+                                            align={isMobile ? "end" : "start"}
+                                        >
+                                            <DropdownMenuItem onClick={() => router.push(`/${item.$id}`)}>
+                                                <Folder />
+                                                <span>Open</span>
+                                            </DropdownMenuItem>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem
+                                                variant="destructive"
+                                                onClick={() =>
+                                                    mutate(
+                                                        { param: { extract_room_id: item.$id } },
+                                                        {
+                                                            onSuccess: () => router.back()
+                                                        }
+                                                    )
                                                 }
-                                            )
-                                        }
-                                    >
-                                        {
-                                            isPending ? (
-                                                <Spinner />
-                                            ) : (<>
-                                                <Trash2 />
-                                                <span>Delete</span>
-                                            </>)
-                                        }
-                                    </DropdownMenuItem>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
-                        </SidebarMenuItem>
-                    ))
-                        :
-                        <SidebarMenuItem className=" text-sm">
-                            No documents here..
-                        </SidebarMenuItem>
-                }
+                                            >
+                                                {
+                                                    isPending ? (
+                                                        <Spinner />
+                                                    ) : (<>
+                                                        <Trash2 />
+                                                        <span>Delete</span>
+                                                    </>)
+                                                }
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </SidebarMenuItem>
+                            ))
+                                :
+                                <SidebarMenuItem className=" text-sm">
+                                    No documents here..
+                                </SidebarMenuItem>
+                        }
+                    </div>
+                </ScrollArea>
                 {documents.length > 5 && (
                     <SidebarMenuItem>
                         <SidebarMenuButton className="text-sidebar-foreground/70" onClick={() => setShowAll(prev => !prev)}>
